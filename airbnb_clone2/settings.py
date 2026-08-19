d7ffc5a1317d8962ad773bb4702337c70e78c960
Lucas Warmling Jasper <warmlingjasper@gmail.com>
2026-08-19 07:44:28 +0100

    fix allowed hosts for render

--- airbnb_clone2/settings.py
+++ airbnb_clone2/settings.py
@@ -29,8 +29,12 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "airbnb-clone.onrender.com"
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
