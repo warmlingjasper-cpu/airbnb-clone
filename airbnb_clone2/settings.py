741980297f676e304a03e2a53047d575fc108ab2
Lucas Warmling Jasper <warmlingjasper@gmail.com>
2026-08-19 08:07:08 +0100

    rename

--- airbnb_clone2/settings.py
+++ airbnb_clone2/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-chave-local-apenas-para-desenvolvimento"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
